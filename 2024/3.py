45b1eec4b44c888ba8a53362c6dc9e5c11526862
limpido <43034953+limpido@users.noreply.github.com>
2024-12-03 15:04:42 +0800

    2024 day3

--- 2024/3.py
+++ 2024/3.py
@@ -2,10 +2,14 @@ import re
 from operator import mul
 
 data = open("input.txt").read().strip()
+
+
+# part 1
 expr = re.findall(r"mul\([0-9]{1,3}\,[0-9]{1,3}\)", data)
 print(sum(list(map(eval, expr))))
 
 
+# part 2
 res = 0
 start, end = 0, -1
 do = re.search(r"do\(\)", data[start:])
@@ -25,4 +29,10 @@ while 0 < start <= len(data):
     end = -1 if dont is None else start + do.end() + dont.start()
     res += sum(list(map(eval, re.findall(r"mul\([0-9]{1,3}\,[0-9]{1,3}\)", data[start+do.end():end]))))
     start = end
+print(res)
+
+
+# part 2 method 2
+data = "".join(part.split("don't()")[0] for part in data.split("do()"))
+res = sum(list(map(eval, re.findall(r"mul\([0-9]{1,3}\,[0-9]{1,3}\)", data))))
 print(res)
