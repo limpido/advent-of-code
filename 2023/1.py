5a52fed4b85b97c055c7eecf744a44d043b56ee5
limpido <43034953+limpido@users.noreply.github.com>
2024-12-03 16:04:56 +0800

    2023 day1

--- 2023/1.py
+++ 2023/1.py
@@ -1,41 +1,18 @@
-def solve1(data):
-    res = 0
-    for line in data:
-        nums = []
-        for c in line:
-            if '0' <= c <= '9':
-                nums.append(c)
-        res += int(nums[0]) * 10 + int(nums[-1])
-    return res
+import re
+
+data = open("input.txt").read().strip().splitlines()
+
+# part 1
+res = sum(int(d[0] + d[-1]) for d in [re.findall(r"\d", line) for line in data])
+print(res)
 
 
-def search(s, target):
-    for i, c in enumerate(target):
-        if i >= len(s) or s[i] != c:
-            return False
-    return True
+# part 2
+digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digit_pattern = re.compile(r"(?=(\d|" + "|".join(digits) + "))")
 
+def word2digit(s):
+    return str(digits.index(s)+1) if len(s) > 1 else s
 
-def solve2(data):
-    digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    res = 0
-    for line in data:
-        nums = []
-        for i, c in enumerate(line):
-            if '0' <= c <= '9':
-                nums.append(c)
-                continue
-            for j, digit in enumerate(digits):
-                if search(line[i:], digit):
-                    nums.append(j+1)
-                    break
-        res += int(nums[0]) * 10 + int(nums[-1])
-
-    return res
-
-
-filename = '../input.txt'
-with open(filename, 'r') as f:
-    data = f.read().strip().splitlines()
-    print(solve1(data))
-    print(solve2(data))
+res = sum(int("".join(map(word2digit, (d[0], d[-1])))) for d in [digit_pattern.findall(line) for line in data])
+print(res)
