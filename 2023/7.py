4ae1b2491d35d0cbaba6fe4662df0ef9c6273349
limpido <43034953+limpido@users.noreply.github.com>
2024-12-05 14:59:21 +0800

    2023 day7

--- 2023/7.py
+++ 2023/7.py
@@ -5,15 +5,13 @@ data = [line.split() for line in open("input.txt").read().splitlines()]
 hand = [d[0] for d in data]
 bid = {d[0]: int(d[1]) for d in data}
 
-USE_JOKER = False
-STRENGTH = "AKQT98765432J" if USE_JOKER else "AKQJT98765432"
 
 class Hand:
     def __init__(self, hand):
         self.hand = hand
         self.count = self.count_hand()
         self.product = reduce(mul, self.count)
-    
+
     def __lt__(self, other):
         if max(self.count) != max(other.count):
             return max(self.count) < max(other.count)
@@ -23,24 +21,27 @@ class Hand:
             if self.hand[i] == other.hand[i]:
                 continue
             return STRENGTH.index(self.hand[i]) > STRENGTH.index(other.hand[i])
-    
+
     def count_hand(self):
         if USE_JOKER:
             count = []
             count_J = 0
             for c in set(self.hand):
-                if c == 'J':
+                if c == "J":
                     count_J = self.hand.count(c)
                 else:
                     count.append(self.hand.count(c))
             if len(count):
                 i = count.index(max(count))
                 count[i] += count_J
-            else: # for case JJJJJ
+            else:  # for case JJJJJ
                 count.append(count_J)
             return count
         else:
             return [self.hand.count(c) for c in set(self.hand)]
 
-hand_objs = list(map(Hand, hand))
-print(sum([mul(bid[h.hand], (i + 1)) for i, h in enumerate(sorted(hand_objs))]))
+
+for USE_JOKER in (False, True):
+    STRENGTH = "AKQT98765432J" if USE_JOKER else "AKQJT98765432"
+    hand_objs = list(map(Hand, hand))
+    print(sum([mul(bid[h.hand], (i + 1)) for i, h in enumerate(sorted(hand_objs))]))
